b3a2568460a7d61deb81cf6b43fd76af13f75498
Aldo <aldoexpert@gmail.com>
2025-08-13 09:55:20 +0700

    fix: cache images

--- app/modules/finance/journal_set/account_form_2.php
+++ app/modules/finance/journal_set/account_form_2.php
@@ -1,4 +1,4 @@
-<?php //ICB0 74:0 81:624                                                      ?><?php //00396
+<?php //ICB0 74:0 81:620                                                      ?><?php //00396
 // ===================================
 //     PT Indonetsoft Meta Digital
 //     Copyright  2013-2025
@@ -7,21 +7,21 @@
 // ===================================
 if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
 ?>
-HR+cPzvpmSxin3dXcSfKxSpJh+o6p23XWXJO0i47DjxEHTh2izH+VKH4ULVhZJ2gGX54fOdq5Vzr
-z11Ow2nvm1NQjBT3RECbyZ4MQ54hp1Fu5Lnjp9P5ifBm7YMSMIIAId3IYUvLGfN/yfp+8UDbL/OD
-UtHoDwNNXGboFVBZ7BvX0ohhJphYRGla8CskL7PiiCJgReOlPGqo4NGozgXtkm1F65aUiq68Yil7
-OlXvfOHrPfCskrtDSMk1Pp/1fr0k0V47vUJ02VR3m142Z/eK3wnytFm555sP6rW3COSAO8I7VBQJ
-hjNHRhnojN/+So5KRXUAmx4q3/X8ttWiqjG/w4u6t1uHoCgaHCIH1nFOZemPQWh78KiO++xwXLDz
-uQgsqPv1GIUMa1ZMuUOqD4nxPJac5S52AQT0RtWD+noBnHpDTh9QbWnieU+SIPI7p7KZYLuxTlFU
-FHJvFy4M+vm/EGDY6MFEOLXJqPXUqSPKZ092rufeOKrUL4WxYPb7z1tzJAAlB566EKtrzIyAT0k3
-fQMPHOBZ05ocJmpSH5MSZ0aTfmTB2CSs2I18Be6GdDBuNrtKjMVKQZKrDalU8xkS7PfzAYuV66NZ
-slYXydKYSIKs5CtxBLCixkEbbcVmOm===
-HR+cPos9pjF4QpFauJTRy19BffBNRyzKiolYTT63ye92Z/bj/SxcuivyPTRErtf/s7j3ojYMTXih
-aOitUuXU1U1O6eejNkDmXO7Mr5AFZTsEHRUNCZ1CzvxH6ASeCU2yd8vyGxem0nsmDPUIIWoKS+o6
-eQyoELGTV2HYUd/sRkb7k6BE+o1+jm25x96yYvGoVPtZ6ZOK1IXZ4TrFGrKMwiclH0Ot4xvDEZIY
-i8bBPdEWTuxmazUzPhRM2O2Zg4VSJMTk/TsKO8zubVN/pDbcSScLJS7Ex4ycVCKHDexs9Q531j8i
-AvUcitIMBGsuHri/1vQpejpP77ZpVSx3dpTPvVWzf2tsbTKrJNHCwpGHRIwnXHaMC3X3/eUoiM3C
-7jUxilQYgdSfoeGZYdcCFLEZwsvg44oI/8r9c9iTlWzhcatNviH9A4MyVvCYjBMNexKMa4duRb1h
-4jKm9t2R78Yo3HYpD5qYvK/eUTzj47Ra1/7lYCAHNNN8iSjhtXXspyrpkM5j+ObXwOUyK5jPhmx2
-cPHlRgmfPqENhZ+R22g6/VeWvZ9jehWD8DQiLDu2fYWc2W15/UrQMWlCD6q/LshYKCVM345bxZ1n
-efW0mjIk160fDxtYOrfqz+Xu4K3xaYsxD6dxVo5GYcI5yzPjhMfvxl4=
+HR+cPxmuUEBu7K7MzO7jmKjfpqTK1Q4UOkg1x8t8yExJfv+Q78YKkZWICGLqCaWUVEYtiSIq0yK7
+PRLBsaQQkKScGS7t28opRWqIIP9s8jQrfK81O6tNzyq2w+EgflXtlKOqxBINvnZcoFkGJXkAEpib
+0sw0ac3zzRhWHohWx3UGoZPCQcBgmgBUO+sDiQ/J6ysEhWN12FFakQo3zQJhBju57Ooq7/7zFcsM
+P/Ei+yHUYeJGAddpr6i830C1+etG1yJ4b2yRSAXphpXMlfiqn2dJ80Ouf7LmmBABtrXGfJM3N4fa
+d/N9TdqGFOaKpFDyjBol/gzT3j+Uk14KgMdz4Wbl/U8dUyeoc5/zp2fN37WxzI6qEpx0muspq8/N
+orKG+hVK7st7xNkSdzv0KPhF0YnBGA1Fddb9hqPugmRxLXcFZ3tViP5+1rzQ2SPqQC7oq1I7ZFyu
+dYjfaaByrpweEP6LQKLr8xyHlOHBie36Zonibs2wMklABlO8ffM7eorE/3RHgFGI+92Jy4A7JOZ4
+6GpzIlYiNPXPUmeFe58J3GvwBfMEICoh3iYuK7kc2CFOQ6qV3QtpvpaQLUBEZ8G/uCOVJ9muXotA
+izTZYphsKQ8wZbjlqtj7e6jlsdq==
+HR+cP/W2YREpqN64EhmSa4Watxl1I/K0AtIMSVwXNkaTQ5l6jAaL7ylowvVFdJD674m++fvEkNLb
+I9aEsfyH85TqG8znWcpFoDYX8dINJfC6qiE4Li8/QdEoOi2OPD8fNAuxVnrpXzQw1W+GlORitVzu
+vM+QUPwLohHrV6Cs7P6+9sxdSa2eWk1OBqD+b8gyVspU4fRVMwi2EAx+oRgPYDzS0/iZhWfdhQHv
+G3gL/nnK3/RgTOK9iegE2EUEtHok9gB1KO0+dgxehgpnQqQ5ovH23gi7cd4mXf1y5rrtI9joNpc2
+fqqXfN7CQAN/4G/TQ6/lOi+oKLGjErddJtqJciyEoE19pNDcmIRgDXvcWcag4QnVK2vwvSqeHKAJ
+qNVs6mfivyQjbB4pMAYnla7QNbsLAQgbB4CTpX4iwjVwz44OrVbl9l3G/lReIfIsf08Kt943sqH0
+sQRIZk+9LaMtUtdfoSq82gQuL+2z7ME2EwW6ozb14Fu3J9FXhFipeP2sJN6JiazZz4YqhegQdRpG
+pBm+CKFCQaxHSMHFzBMaDvnDC2jaoGrfMdGqU45ctqpLs1lusTESmJa9ZE6WbRK5fIL9ud+9auLE
+bSsMrHic0OZ4lQFPYinl0wqHal76hblL5axkFOFh/WkZZmqA2KxqjO7Kli2RZAj/WyQl
